22f12fb38fffcdf19c849affa0a82c53cade841e
Ricky Ultimate <rickyogigi@gmail.com>
2026-04-30 22:47:09 +0100

    fix(backend): correct PrismaPg instantiation to use pg Pool

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({
+  connectionString: process.env["DATABASE_URL"],
+});
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
